705f76e6d6c05810d1f5482a77000b83fea6b718
bishwajit <bishwajit2810@gmail.com>
2024-09-13 19:48:04 +0600

    string func

---
+++ string/str_func.py
@@ -0,0 +1,16 @@
+str = "bishwajit"
+
+print(len(str))
+print(str.endswith("jit"))
+print(str.startswith("Bis"))
+print(str.capitalize())
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.find("i"))
+print(str.replace("i","l"))
+print(str.replace("l","i"))
+
+
+
+
--- string/string.py
+++ string/string.py
@@ -4,9 +4,24 @@ print(hello.upper())
 print(hello.lower())
 print(hello.capitalize())
 print(hello.count("l"))
+print(len(hello))
 
+sl = hello[0:3]
+print(sl)
+
+sl = hello[-4:-2]
+print(sl)
 
 y=5
 z=" world"
 print(hello*y)
 print(hello + z)
+
+print()
+hello='''djfjdlfhds sdfjldsjf dsffldsjfj
+ds;fjldsjfjdsklj
+dsfjdsjfjdsjf
+sdfjldsjfjds
+dfjdasljfjdsjf
+sdfhjdsfhjdsjj'''
+print(hello)
